e0e5a08680dd47ad9c69c5568d51b7b378f82194
jazzytoor <32848916+jazzzyjeff@users.noreply.github.com>
2026-05-03 19:18:00 +0100

    chore: create basic vpc

---
+++ aws/lambda/s3-file-system/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "service" {
+  type    = string
+  default = "s3-file-system"
+}
+
+variable "region" {
+  type = string
+}
---
+++ aws/lambda/s3-file-system/terraform/vpc.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.5"
+
+  name = "${var.service}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.default_tags
+}
+
+locals {
+  private_subnet_by_az = zipmap(
+    module.vpc.azs,
+    module.vpc.private_subnets
+  )
+}
